eb2dd64e902b9d1c903ee7e0c1d16a4ddedbf8d9
gulci <bigmariobro@gmail.com>
2015-01-10 22:49:34 -0800

    Renamed index to signin for clarity ;)

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ module.exports = function(passport){
 	/* GET login page. */
 	router.get('/', function(req, res) {
     	// Display the Login page with any flash message, if any
-		res.render('index', { message: req.flash('message') });
+		res.render('signin', { message: req.flash('message') });
 	});
 
 
