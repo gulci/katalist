5206ca493aadf5b3cf7b9d36bb335d009e9b637e
Michael Zhang <failed.down@gmail.com>
2015-01-17 06:53:21 -0500

    pulls from firebase

---
+++ public/js/katalist.js
@@ -0,0 +1,17 @@
+var ref = new Firebase("https://katalist.firebaseio.com");
+
+ref.on("value", function(snapshot) {
+	var obj = snapshot.val();
+	for(var ind in obj) {
+		var html = "<div class='well well-lg' id='item-"+ind+"' style='display:none;'>";
+		html += "<h2>" + obj[ind].title + "</h2>";
+		html += "<h4><a href='" + obj[ind].url + "' target='_blank'>" + obj[ind].url + "</a></h4>";
+		html += "<button class='upvote btn btn-default' type='button'><span class='glyphicon glyphicon-chevron-up'></span> "+obj[ind].numVotes+"</button>";
+		html += "</div>";
+		// console.log(html);
+		$("#post_container").prepend(html);
+		$("#item-"+ind).slideDown("fast");
+	}
+}, function (errorObject) {
+	console.log("The read failed: " + errorObject.code);
+});
