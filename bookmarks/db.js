a02228e59e823e4dedeebac336230b9dd111a835
Brett Martin <bcmjnk@gmail.com>
2015-01-17 00:37:20 -0500

    initial commit

---
+++ bookmarks/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+	'url' : 'mongodb://main:user@ds031581.mongolab.com:31581/bookmarks'
+	//'url' : 'mongodb://localhost/test'
+}
---
+++ bookmarks/routes/index.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
+
+module.exports = function(passport){
+
+	/* GET login page. */
+	router.get('/', function(req, res) {
+    	// Display the Login page with any flash message, if any
+		res.render('index', { message: req.flash('message') });
+	});
+
+
+
+	/* Handle Login POST */
+	router.post('/login', passport.authenticate('login', {
+		successRedirect: '/home',
+		failureRedirect: '/',
+		failureFlash : true
+	}));
+
+	router.get('/ext', function(req, res) {
+    	// Display the Login page with any flash message, if any
+		res.render('extension');
+	});
+
+
+	router.post('/testpost', function(req, res){
+	//console.log(req.params.hello);
+		var number = req.param('hello');
+		console.log("value of number: " + number);
+		res.send(number);
+	});
+
+
+
+	/* GET Registration Page */
+	router.get('/signup', function(req, res){
+		res.render('register',{message: req.flash('message')});
+	});
+
+	/* Handle Registration POST */
+	router.post('/signup', passport.authenticate('signup', {
+		successRedirect: '/home',
+		failureRedirect: '/signup',
+		failureFlash : true
+	}));
+
+	// Handle logout
+	router.get('/logout', function(req, res){
+		  req.logout();
+		    res.redirect('/');
+	});
+
+	/* GET Home Page */
+	router.get('/home', isAuthenticated, function(req, res){
+		res.render('home', { user: req.user });
+	});
+
+	/* Handle Logout */
+	router.get('/signout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	return router;
+}
+
+
+
+
+
