62df7b3351a51f373bd669d2a09c8905f1a79f51
Michael Zhang <failed.down@gmail.com>
2015-01-17 06:56:07 -0500

    fixed duplicate issue

--- public/js/katalist.js
+++ public/js/katalist.js
@@ -1,17 +1,23 @@
 var ref = new Firebase("https://katalist.firebaseio.com");
 
+var inds = [];
+
 ref.on("value", function(snapshot) {
 	var obj = snapshot.val();
 	for(var ind in obj) {
-		var html = "<div class='well well-lg' id='item-"+ind+"' style='display:none;'>";
-		html += "<h2>" + obj[ind].title + "</h2>";
-		html += "<h4><a href='" + obj[ind].url + "' target='_blank'>" + obj[ind].url + "</a></h4>";
-		html += "<button class='upvote btn btn-default' type='button'><span class='glyphicon glyphicon-chevron-up'></span> "+obj[ind].numVotes+"</button>";
-		html += "</div>";
-		// console.log(html);
-		$("#post_container").prepend(html);
-		$("#item-"+ind).slideDown("fast");
+		if (inds.indexOf(ind) == -1) {
+			var html = "<div class='well well-lg' id='item-"+ind+"' style='display:none;'>";
+			html += "<h2>" + obj[ind].title + "</h2>";
+			html += "<h4><a href='" + obj[ind].url + "' target='_blank'>" + obj[ind].url + "</a></h4>";
+			html += "<button class='upvote btn btn-default' type='button'><span class='glyphicon glyphicon-chevron-up'></span> "+obj[ind].numVotes+"</button>";
+			html += "</div>";
+			// console.log(html);
+			$("#post_container").prepend(html);
+			$("#item-"+ind).slideDown("fast");
+			inds.push(ind);
+		}
 	}
+	console.dir(inds);
 }, function (errorObject) {
 	console.log("The read failed: " + errorObject.code);
 });
